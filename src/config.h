45fccc58ee3ba80dd02624fc51a6ed136a374c01
el-coda-coda <codarin.andrea@gmail.com>
2021-01-22 18:16:37 +0100

    file config with MOTORS and COM.

---
+++ src/config.h
@@ -0,0 +1,17 @@
+//In this file we set some general settings
+
+//MOTORS
+
+#define PWR_A   3
+#define PWR_B   11
+
+#define DIR_A   4
+#define DIR_B   12
+
+//COMUNICATION
+
+String dir;
+
+String LEFT = "LEFT";
+String RIGHT = "RIGHT";
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include<config.h>
 
 void setup() {
   // put your setup code here, to run once:
