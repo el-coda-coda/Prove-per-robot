3714b4ba90ad256c6fb834e66df90f2196de3db9
el-coda-coda <codarin.andrea@gmail.com>
2021-01-25 19:34:21 +0100

    aggiunto segmentDegSet, che verifica come sta deg

--- src/segments.h
+++ src/segments.h
@@ -1,10 +1,30 @@
-string segmentCURVE(int deg, String direction)
+int segmentCURVE(int deg, String direction)
 {
     engineON(enginePWR, direction);
     compassDegDelay(deg, direction);
-    engineOFF()
+    engineOFF();
 
     if(LOG_LEVEL == VERBOSE)    log(String("SegmentCURVE OFF"));
-    return String("SegmentCURVE FINISHED");
+    return compassReadMedia();
 }
 
+bool segmentDegSet()
+{
+    unsigned int setDeg = compassReadMedia();
+    int setDegRight = engineCURVE(10, RIGHT);
+    bool segmentSituation;
+
+    if ((setDegRight < setDeg) || (setDegRight > (setDeg + 180))
+    {
+        if (LOG_LEVEL == INFO)  log(String("A DETRA SONO DI MENO"));
+        segmentSituation = true;
+    }
+    else
+    {
+        if (LOG_LEVEL == INFO)  log(String("A SINISTRA SONO DI MENO"));
+        segmentSituation = false;
+    }
+    engineCURVE(10, LEFT);
+
+    return segmentSituation;
+}
