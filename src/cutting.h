3c3786fa42535e583dd208c88a2f5a67527f074b
el_coda_coda <codarin.andrea@gmail.com>
2021-06-30 18:30:30 +0200

    little fixes

--- src/cutting.h
+++ src/cutting.h
@@ -7,20 +7,20 @@ class cut {
 };
 
 void cut::off() { //motor cut stops
-    if(CUT_ENABLED) esc.writeMicroseconds(CUT_OFF);
+    if(cut_enabled) esc.writeMicroseconds(CUT_OFF);
     else write.info(String("Cutting is not enabled"));
 }
 
 void cut::on (int speed) { //motor cut rotate from MIN to MAX
     int cut_speed = map((speed * cutterInputScale), 0, 100, CUT_OFF, CUT_ON);
-    if(CUT_ENABLED) esc.writeMicroseconds(cut_speed);
+    if(cut_enabled) esc.writeMicroseconds(cut_speed);
     else write.info(String("Cutting is not enabled"));
 }
 
 void cut::setup (){ //setup for the esc module
     write.info(String("CUTTER SETUP"));
     esc.writeMicroseconds(CUT_OFF);
-    if(CUT_ENABLED){
+    if(cut_enabled){
         delay(1000);
         esc.writeMicroseconds(CUT_OFF);
         delay(3000);
