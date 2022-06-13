b1c127b72792f658a76e8a17939bd2b8ef9dab95
el_coda_coda <codarin.andrea@gmail.com>
2022-06-13 22:09:24 +0200

    prove-per-robot1.1 riadattamento software

--- src/config.h
+++ src/config.h
@@ -1,77 +1,13 @@
-//In this file we set some general settings
-
-//ENGINES
-
 #define PWR_A   3
 #define PWR_B   11
-
 #define DIR_A   4
 #define DIR_B   12
 
-#define WHEEL_DIAM  12 //in centimeters
-#define ROTATION_SPEED  0.33 // 20 RPM, 0.33333 rotations for second
+int engineThereshold = 100;
+int leftEnginePerc = 100/100;
+int rightEnginePerc = 100/100;
 
-unsigned int enginePWR = 100;
-int leftEngineDiff = 0;
-int rightEngineDiff = 0;
-
-//CUTTER
-#define CUTTER_PIN  8
-#define CUTTER_AMP_PIN  1 //the analog pin where read the consumption of the motor for cutting
-#define CUT_OFF 1000
-#define CUT_ON_MAX  2000
-
-int cutterOffset = 0;
-float cutterScale = 0.185;  //motor cut current scale V/A
-float cutterInputScale = 0.88;
-bool cut_enabled  = false;
-
-//SOLAR PANEL
-#define SWITCH_PANEL_PIN   7
-#define READ_PANEL_PIN  2
-#define PANEL_SCALE 0.185
-
-float panelOffset = 0;
-
-//BATTERY
-#define BAT_PIN  3
-#define BAT_MIN  9.0
-#define BAT_MAX 13.8
-#define BAT_SCALE   0.054
-
-//ULTRASONIC SENSORS
-#define TRIG_PIN_1  5
-#define TRIG_PIN_2  6
-#define ECHO_PIN_1  2
-#define ECHO_PIN_2  13
-
-#define US_OUTRANGE 38000
-#define US_SCALE    0.034
-#define NO_OBSTACLE 1000
-
-bool ultrasonic_enabled = true;
-int stop_distance = 70;
-
-//QMC5883L
-int deg_threshold;
-
-//COMUNICATION
-#define INFO    3
-#define DEBUG   2
-#define VERBOSE 1
-String dir;
 String LEFT = "LEFT";
 String RIGHT = "RIGHT";
 String STRAIGHT = "STRAIGHT";
 String BACK = "BACK";
-int logLevel;
-String FLAT = "FLAT";
-String CUSTOM = "CUSTOM";
-bool LOG_SERIAL = true; // or OFF for don't log on the serial
-bool LOG_DISPLAY = false; // or OFF for don't log on the display
-
-//MATHS
-#define RADIANTS_TO_DEG 57.30     // 1 deg = 0.0174533 radians
-
-//DIMENSIONS OF THE ROBOT
-#define WIDTH   40
--- src/main.cpp
+++ src/main.cpp
@@ -1,76 +1,9 @@
 #include <Arduino.h>
-#include <Wire.h>
-#include <Servo.h>
-#include <EEPROM.h>
-#include <QMC5883LCompass.h>
-#include <LiquidCrystal_I2C.h>
-#include <config.h>
-#include <log.h>
-#include <calculates.h>
-#include <sensors.h>
-#include <ultrasonic.h>
-#include <engines.h>
-#include <QMC5883.h>
-#include <segments.h>
-#include <cutting.h>
-#include <auto-drive.h>
 
-String command = "";
 void setup(){
-  Serial.begin(115200);
-  pinMode(SWITCH_PANEL_PIN, OUTPUT);
-  digitalWrite(CUTTER_PIN, LOW);
-  logLevel = INFO;
-  Wire.begin(); 
-  esc.attach(CUTTER_PIN);
-  cutter.setup();
-  engineSET();
-  sensor.setup();
-  sensor.setCompass(CUSTOM);
-  Serial.println(String(sensor.ultrasonic(TRIG_PIN_1, ECHO_PIN_1)));
-  write.info(String("BAT: ") + String(sensor.battery()) + String(" %"));
-  delay(2000);
+
 }
 
-void loop() {
-  if(Serial.available()){
-    command = Serial.readString();
-    Serial.print("[command] " + String(command));
-    if (command.startsWith("-STRAIGHT")) write.info(String(segmentStraight(STRAIGHT, 100)));
-    if (command.startsWith("-BACK")) write.info(String(segmentStraight(BACK, 100)));
-    if (command.startsWith("-RIGHT")){
-      int x = (command.substring(6).toInt());
-      write.info(String(command.substring(6)));
-      segmentCURVE(x, RIGHT, sensor.compass());
-    }
-    if (command.startsWith("-LEFT")){
-      int x = (command.substring(5).toInt());
-      write.info(String(command.substring(5)));
-      segmentCURVE(x, LEFT, sensor.compass());
-    }
-    if (command.startsWith("-STOP")) engineOFF();
-    if (command.startsWith("-ENGINE POWER")){
-      int x = (command.substring(13).toInt());
-      write.info("POWER: " + String(x) + "%");
-      enginePWR = x;
-    }
-    if (command.startsWith("-CALIBRATION")){
-      engineON(enginePWR, RIGHT);
-      qmcCalibration();
-      engineOFF();
-      firstCompass();
-    }
-    if (command.startsWith("-CUT OFF")) cutter.off();
-    if (command.startsWith("-CUT ON")){
-      int x = (command.substring(7).toInt());
-      write.info("POWER: " + String(x) + "%");
-      cutter.on(x);
-    }
-    if (command.startsWith("-COMPASS")) write.info(String(sensor.compass()));
-    if (command.startsWith("-FIRST COMPASS")) firstCompass();
-    if (command.startsWith("-PANEL AMP")) write.info(String(sensor.panelAmp()));
-    if (command.startsWith("-PANEL VOLT"))  write.info(String(sensor.panelVolts()));
-    if (command.startsWith("-ANGLE US"))  write.info(angleUS(sensor.ultrasonic(TRIG_PIN_1, ECHO_PIN_1), sensor.ultrasonic(TRIG_PIN_2, ECHO_PIN_2)));
-    if (command.startsWith("-AUTO DRIVE")) randomDrive(100);
-  }
+void loop(){
+
 }
